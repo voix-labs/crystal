a2a6a6800315f3eb0cb22b74c7fdb235d5994629
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-02-10 01:29:13 +0700

    feat: add shared packages from @tutur3u/monorepo (wip)

---
+++ src/app/api/ai/chat/google/route.ts
@@ -0,0 +1,9 @@
+import { POST } from '@tutur3u/ai/chat/google/route';
+
+export const config = {
+  maxDuration: 60,
+  preferredRegion: 'sin1',
+  runtime: 'edge',
+};
+
+export { POST };
---
+++ src/app/api/ai/chat/google/summary/route.ts
@@ -0,0 +1,9 @@
+import { PATCH } from '@tutur3u/ai/chat/google/summary/route';
+
+export const config = {
+  maxDuration: 60,
+  preferredRegion: 'sin1',
+  runtime: 'edge',
+};
+
+export { PATCH };
