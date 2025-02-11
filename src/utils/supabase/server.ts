9040b60de83610907955c14d1cd7009dec14b148
Võ Hoàng Phúc <contact@vohoangphuc.com>
2025-02-12 00:55:41 +0700

    feat: integrate Supabase client utilities and update configuration for local development

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,27 +1,55 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createServerClient } from "@supabase/ssr";
+import type { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { cookies } from "next/headers";
+import { SupabaseCookie, checkEnvVariables } from "./common";
 
-export async function createClient() {
-    const cookieStore = await cookies()
+function createCookieHandler(cookieStore: ReadonlyRequestCookies) {
+  return {
+    getAll() {
+      return cookieStore.getAll();
+    },
+    setAll(cookiesToSet: SupabaseCookie[]) {
+      try {
+        cookiesToSet.forEach(({ name, value, options }) =>
+          cookieStore.set(name, value, options)
+        );
+      } catch {
+        // The `setAll` method was called from a Server Component.
+        // This can be ignored if you have middleware refreshing
+        // user sessions.
+      }
+    },
+  };
+}
 
-    return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return cookieStore.getAll()
-                },
-                setAll(cookiesToSet) {
-                    try {
-                        cookiesToSet.forEach(({ name, value, options }) =>
-                            cookieStore.set(name, value, options)
-                        )
-                    } catch (error) {
-                        console.error('Failed to set cookies:', error)
-                    }
-                },
-            },
+async function createGenericClient(isAdmin: boolean) {
+  const { url, key } = checkEnvVariables({ useServiceKey: isAdmin });
+  const cookieStore = await cookies();
+  return createServerClient(url, key, {
+    cookies: isAdmin
+      ? {
+          getAll() {
+            return [] as SupabaseCookie[];
+          },
+          // eslint-disable-next-line no-unused-vars
+          setAll(_: SupabaseCookie[]) {},
         }
-    )
+      : createCookieHandler(cookieStore),
+  });
+}
+
+export const createAdminClient = () => {
+  return createGenericClient(true);
+};
+
+export function createClient() {
+  return createGenericClient(false);
+}
+
+export async function createDynamicClient() {
+  const { url, key } = checkEnvVariables({ useServiceKey: false });
+  const cookieStore = await cookies();
+  return createServerClient(url, key, {
+    cookies: createCookieHandler(cookieStore),
+  });
 }
