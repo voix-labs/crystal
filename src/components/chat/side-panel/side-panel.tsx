ac9e63150c5317150f0c04b4aa81403aa35d9762
mudoker <huuquoc7603@gmail.com>
2025-02-15 16:38:40 +0700

    feat: remove copyright comments and add "use client" directive in multiple files

--- src/components/chat/side-panel/side-panel.tsx
+++ src/components/chat/side-panel/side-panel.tsx
@@ -1,20 +1,5 @@
-/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 "use client";
-import Logger, { LoggerFilterType } from '@/components/chat/logger/Logger';
+import Logger, { LoggerFilterType } from '@/components/chat/logger/logger';
 import { cn } from '@/lib/utils';
 import { useLiveAPIContext } from '@contexts/LiveAPIContext';
 import { useLoggerStore } from '@lib/store-logger';
@@ -105,8 +90,8 @@ export default function SidePanel() {
                             backgroundColor: isFocused
                                 ? 'var(--Neutral-30)'
                                 : isSelected
-                                  ? 'var(--Neutral-20)'
-                                  : undefined,
+                                    ? 'var(--Neutral-20)'
+                                    : undefined,
                         }),
                     }}
                     defaultValue={selectedOption}
